936ae2fc3feda58f2680a1720bcbc98d7bc530c3
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2020-11-10 16:14:36 +1100

    edit notes in test stub

--- test/test.rs
+++ test/test.rs
@@ -1,9 +1,10 @@
 // SYNTAX TEST "source.rust" "Textmate grammar scope tests"
 
-// This file is currently a stub, and will eventually be expanded to unit test all scopes.
+// This file is a placeholder stub that will gradually be expanded to unit test all scopes.
 // Instructions for writing Textmate grammar tests can be found at:
 // https://github.com/PanAeon/vscode-tmgrammar-test/blob/master/README.md
 
+// example test
 extern crate std;
 // <------ meta.import.rust storage.type.rust
 //     ^^^^^ meta.import.rust keyword.other.crate.rust
