cbd45f7eee0dc0b1285717e6ee8d3aa2b8bab9e5
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2020-11-14 19:18:36 +1100

    notes explaining the stall in testing

--- test/test.rs
+++ test/test.rs
@@ -1,5 +1,7 @@
 // SYNTAX TEST "source.rust" "Textmate grammar scope tests"
 
+// PLEASE NOTE: My apologies, but we are blocked on testing, due to an inability of the test platform to process some of the lookbehinds in the grammar. VS Code has no issue with them, but the test engine cannot create the Oniguruma scanner needed to run the tests.
+
 // This file is a placeholder stub that will gradually be expanded to unit test all scopes.
 // Instructions for writing Textmate grammar tests can be found at:
 // https://github.com/PanAeon/vscode-tmgrammar-test/blob/master/README.md
