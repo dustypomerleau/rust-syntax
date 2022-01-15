61230974e370b9c375c1aeb6ab83bd9d334e5f87
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2022-01-15 14:50:01 +0100

    Fix floats syntax highlighting

--- test/test.rs
+++ test/test.rs
@@ -10,3 +10,39 @@ extern crate std;
 //     ^^^^^ meta.import.rust keyword.other.crate.rust
 //           ^^^ meta.import.rust
 //              ^ meta.import.rust punctuation.semi.rust
+
+// decimal floats test
+let x1 = 1.123e12;
+//       ^^^^^^^^ constant.numeric.decimal.rust
+//        ^ punctuation.separator.dot.decimal.rust
+//            ^ keyword.operator.exponent.rust
+//             ^^ constant.numeric.decimal.exponent.mantissa.rust
+let x2 = 1.123e+12;
+//       ^^^^^^^^^ constant.numeric.decimal.rust
+//        ^ punctuation.separator.dot.decimal.rust
+//            ^ keyword.operator.exponent.rust
+//             ^ keyword.operator.exponent.sign.rust
+//              ^^ constant.numeric.decimal.exponent.mantissa.rust
+let x3 = 1.123e-12;
+//       ^^^^^^^^^ constant.numeric.decimal.rust
+//        ^ punctuation.separator.dot.decimal.rust
+//            ^ keyword.operator.exponent.rust
+//             ^ keyword.operator.exponent.sign.rust
+//              ^^ constant.numeric.decimal.exponent.mantissa.rust
+let x4 = 1.123E12;
+//       ^^^^^^^^ constant.numeric.decimal.rust
+//        ^ punctuation.separator.dot.decimal.rust
+//            ^ keyword.operator.exponent.rust
+//             ^^ constant.numeric.decimal.exponent.mantissa.rust
+let x5 = 1.123E+12;
+//       ^^^^^^^^^ constant.numeric.decimal.rust
+//        ^ punctuation.separator.dot.decimal.rust
+//            ^ keyword.operator.exponent.rust
+//             ^ keyword.operator.exponent.sign.rust
+//              ^^ constant.numeric.decimal.exponent.mantissa.rust
+let x6 = 1.123E-12;
+//       ^^^^^^^^^ constant.numeric.decimal.rust
+//        ^ punctuation.separator.dot.decimal.rust
+//            ^ keyword.operator.exponent.rust
+//             ^ keyword.operator.exponent.sign.rust
+//              ^^ constant.numeric.decimal.exponent.mantissa.rust
