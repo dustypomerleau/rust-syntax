c7288f7debcfaa0817d8cccfe6e8b041c5bbe4e6
Wilfred Hughes <me@wilfred.me.uk>
2026-08-24 21:03:01 +0100

    Highlight f16 and f128

---
+++ test/test_extended_floats.rs
@@ -0,0 +1,8 @@
+// SYNTAX TEST "source.rust" "f16 and f128 highlighting"
+
+    let half = 1.5f16;
+//             ^^^^^^ constant.numeric.decimal.rust
+//                ^^^ entity.name.type.numeric.rust
+    let quad: f128 = 1f128;
+//            ^^^^ entity.name.type.numeric.rust
+//                   ^^^^^ constant.numeric.decimal.rust
