26e06343babb23d1c805a5dc77ce9e5908943cd8
Wilfred Hughes <me@wilfred.me.uk>
2026-08-24 23:33:01 +0100

    test: cover simple function highlighting

---
+++ test/test_function.rs
@@ -0,0 +1,22 @@
+// SYNTAX TEST "source.rust" "Simple function highlighting"
+
+    fn add(left: u32, right: u32) -> u32 {
+//  ^^ keyword.other.fn.rust
+//     ^^^ entity.name.function.rust
+//        ^ punctuation.brackets.round.rust
+//         ^^^^ variable.other.rust
+//             ^ keyword.operator.key-value.rust
+//               ^^^ entity.name.type.numeric.rust
+//                  ^ punctuation.comma.rust
+//                    ^^^^^ variable.other.rust
+//                         ^ keyword.operator.key-value.rust
+//                           ^^^ entity.name.type.numeric.rust
+//                                ^^ keyword.operator.arrow.skinny.rust
+//                                   ^^^ entity.name.type.numeric.rust
+//                                       ^ punctuation.brackets.curly.rust
+        left + right
+//      ^^^^ variable.other.rust
+//           ^ keyword.operator.math.rust
+//             ^^^^^ variable.other.rust
+    }
+//  ^ punctuation.brackets.curly.rust
